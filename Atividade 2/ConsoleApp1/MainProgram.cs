aca7ed3d25ccb06f9f7544d9e2826aa7f96b25aa
KICKVALE <122411222+KICKVALE@users.noreply.github.com>
2024-08-02 08:43:39 -0300

    Add files via upload

---
+++ Atividade 2/ConsoleApp1/MainProgram.cs
@@ -0,0 +1,14 @@
+﻿using MinhaEmpresa.Cliente;
+using MinhaEmpresa.Produto;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Cliente cliente = new Cliente("João Silva", "joao.silva@exemple.com");
+        Produto produto = new Produto("Agua");
+
+        Console.WriteLine($"Cliente: {cliente.Nome} \nEmail: {cliente.Email} \nProduto: {produto.Produtos}");
+    }
+}
